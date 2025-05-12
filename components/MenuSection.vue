e983c1c63f1155b0be37ad7dd7abca62cd129d8d
Votre Nom <befrigeo@gmail.com>
2025-05-12 11:56:31 +0200

    Mise à jour du site avec les dernières modifications 4

--- components/MenuSection.vue
+++ components/MenuSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="my-8">
-    <h2 class="text-2xl font-serif border-b-2 border-gray-300 pb-2 mb-4">{{ title }}</h2>
+    <h2 class="text-3xl font-extrabold font-serif border-b-2 border-gray-300 pb-2 mb-4">{{ title }}</h2>
     <div class="grid gap-6">
       <MenuItem
         v-for="(item, index) in items"
