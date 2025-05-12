4a4a91eb070fd91568e0a3d7b6cfb1fc5b802c8d
Votre Nom <befrigeo@gmail.com>
2025-05-12 11:48:18 +0200

    Mise à jour du site avec les dernières modifications 3

--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex flex-col sm:flex-row justify-between items-start py-4 border-b border-gray-600">
-    <div class="flex-1">
+  <div class="flex justify-between items-start py-4 border-b border-gray-600">
+    <div class="w-3/4 pr-4">
       <h3 class="text-lg font-semibold">{{ name }}</h3>
-      <p class="text-sm text-white-700">{{ description }}</p>
+      <p class="text-sm text-white-700 break-words">{{ description }}</p>
     </div>
-    <span class="text-md font-bold text-white-800 mt-2 sm:mt-0 sm:text-right">{{ price }} €</span>
+    <div class="w-1/4 text-right">
+      <span class="text-md font-bold text-white-800">{{ price }} €</span>
+    </div>
   </div>
 </template>
 
@@ -12,15 +14,6 @@
 defineProps({
   name: String,
   description: String,
-  price: String
+  price: [String, Number]
 });
 </script>
-
-<style scoped>
-/* Assurer le retour à la ligne dans la description sur mobile */
-@media (max-width: 767px) {
-  .menu-item p {
-    white-space: normal; /* Permet à la description de se casser sur plusieurs lignes */
-  }
-}
-</style>
