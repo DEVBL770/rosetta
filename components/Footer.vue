a65a6713ab9abd4ab959db525c15a007f09961bf
Votre Nom <befrigeo@gmail.com>
2025-05-13 14:47:27 +0200

    Mise à jour du site avec les dernières modification 13

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,14 @@
 <template>
-  <footer class="bg-gray-700 border-t border-gray-700 text-center text-l text-gray-100 py-6 space-y-1 font-black-paris">
-    <p>Jacob Meatpacker</p>
-    <p>5 rue Médéric, 75017 Paris — 01 56 33 70 00</p>
+  <footer class="bg-gray-700 border-t border-gray-700 text-center text-l text-gray-100 py-6 space-y-2 font-black-paris">
+    <div>
+      <img
+        src="https://static.wixstatic.com/media/a99bfb_9c7e97010cbb487ea470ca08921507d7~mv2.png/v1/fill/w_146,h_85,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/image001%20(2)_edited.png"
+        alt="Logo Jacob Meatpacker"
+        class="mx-auto mb-2 w-20 h-auto"
+      />
+    </div>
+    <b><p>Jacob Meatpacker</p></b>
+    <b><p>5 rue Médéric, 75017 Paris — 01 56 33 70 00</p></b>
   </footer>
 </template>
 
