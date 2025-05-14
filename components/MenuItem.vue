dcc4782d5f5cda6448d44cedc75df0e8f560ad7f
Votre Nom <befrigeo@gmail.com>
2025-05-14 13:00:46 +0200

    Initial commit 2

--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -2,10 +2,10 @@
   <div class="border-b border-[#e7e2dd] pb-4">
     <div class="flex justify-between items-start">
       <div>
-        <h3 class="text-lg font-medium text-[#1b3b34]">{{ name }}</h3>
+        <h3 class="text-lg font-medium text-[#18534f]">{{ name }}</h3>
         <p class="text-sm text-[#555] italic">{{ description }}</p>
       </div>
-      <span class="text-[#a16c4f] font-semibold whitespace-nowrap">{{ price }}</span>
+      <span class="text-[#cc9fbb] font-semibold whitespace-nowrap">{{ price }}</span>
     </div>
   </div>
 </template>
